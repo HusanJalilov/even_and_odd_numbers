b392e44d7d81e34a430956bce8989aaa312f038c
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-20 10:18:34 +0500

    task3

--- number_even.py
+++ number_even.py
@@ -1,5 +1,6 @@
 #A four-digit integer is given. Find the number of even digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int=2345
 #Print the number of even digits in the variable "var_int".
+print(4-var_int//1000%2-var_int//100%2-var_int//10%2-var_int%2)
