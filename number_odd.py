64b8c8113ed9cde85dc3410949e519b1fd82866c
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-20 10:26:27 +0500

    task 04

--- number_odd.py
+++ number_odd.py
@@ -1,5 +1,6 @@
 #A four-digit integer is given. Find the number of odd digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int=3333
 #Print the number of odd digits in the variable "var_int".
+print(4-var_int//1000%2+1-var_int//100%2+1-var_int//10%2+1-var_int%2+1)
